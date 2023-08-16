f39ffb52aa34befb0e737106c8590fcfc7683a87
David Andrianarivo <andrianarivo.david@gmail.com>
2023-08-16 21:49:16 +0300

    created models to process data

---
+++ app/models/like.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class Like < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  belongs_to :post
+
+  before_save :update_likes_counter
+
+  def update_likes_counter
+    post.update(likes_counter: post.likes.count)
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class User < ApplicationRecord
+  has_many :posts, foreign_key: :author_id
+  has_many :comments, foreign_key: :author_id
+  has_many :likes, foreign_key: :author_id
+
+  def three_most_recent_posts
+    posts.order(created_at: :desc).limit(3)
+  end
+end
