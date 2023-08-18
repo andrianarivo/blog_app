49897e5de025a89a6fbade2e375dc73fdde84f2a
David Andrianarivo <andrianarivo.david@gmail.com>
2023-08-18 22:23:24 +0300

    rubocop

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,7 +2,10 @@ require 'rails_helper'
 
 describe Post, type: :model do
   @user = User.new(name: 'Emmy', photo: 'https://photo.url', bio: 'My Second Bio', posts_counter: 0)
-  subject { Post.new(author: @user, title: 'Post title for fun', text: 'Lorem Ipsum Dolor sit amet. Lorem Ipsum Dolor sit amet.') }
+  subject do
+    Post.new(author: @user, title: 'Post title for fun',
+             text: 'Lorem Ipsum Dolor sit amet. Lorem Ipsum Dolor sit amet.')
+  end
 
   before { subject.save }
 
@@ -36,7 +39,7 @@ describe Post, type: :model do
   it '#five_recente_comments returns correct value' do
     new_user = User.create(name: 'Salim', photo: 'https://photo.url', bio: 'I am a good boy', posts_counter: 0)
     new_post = Post.create(author: new_user, title: 'This is yours', text: 'Lorem Ipsum again!',
-                comments_counter: 0, likes_counter: 0)
+                           comments_counter: 0, likes_counter: 0)
     expect(new_post.five_recent_comments.length).to be <= 5
   end
 end
