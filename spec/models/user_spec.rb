2f3d48560725cf33cf0d2952ee56b147021fe626
David Andrianarivo <andrianarivo.david@gmail.com>
2023-08-18 22:19:08 +0300

    build test for models

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,42 +1,30 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  subject { User.new(id: 1, name: 'Ali') }
+describe User, type: :model do
+  subject { User.new(name: 'Evah', photo: 'http://photo.url', bio: 'My Bio Fake', posts_counter: 0) }
 
   before { subject.save }
 
-  describe 'validation tests' do
-    it 'name should be present' do
-      subject.name = nil
-      expect(subject).to_not be_valid
-    end
-
-    it 'posts_counter should be integer' do
-      subject.posts_counter = 'hey'
-      expect(subject).to_not be_valid
-    end
-
-    it 'posts_counter should be greater than or equal to zero' do
-      subject.posts_counter = -2
-      expect(subject).to_not be_valid
-      subject.posts_counter = 0
-      expect(subject).to be_valid
-    end
+  it 'should be valid' do
+    expect(subject).to be_valid
   end
 
-  describe '#three_most_recent_posts' do
-    it 'returns the 3 most recent posts' do
-      # Arrange
-      user = User.create(id: 2, name: 'David')
-      post1 = Post.create(title: 'post1', author: subject, created_at: 3.day.ago)
-      post2 = Post.create(title: 'post2', author: subject, created_at: 2.day.ago)
-      post3 = Post.create(title: 'post3', author: subject, created_at: 1.day.ago)
+  it 'should have a name' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
 
-      # Act
-      recent_posts = user.three_most_recent_posts
+  it '#posts_counter is greater than or equal to 0' do
+    subject.posts_counter = -1
+    expect(subject).to_not be_valid
+  end
 
-      # Assert
-      expect(recent_posts).to eq([post3, post2, post1])
-    end
+  it '#posts_conter should be an integer' do
+    subject.posts_counter = 'one'
+    expect(subject).to_not be_valid
+  end
+
+  it '#three_most_recent_posts returns correct value' do
+    expect(subject.three_most_recent_posts.length).to be <= 3
   end
 end
