10b4473a3b79465a8d15eb7cba8f6737d7fa0b74
David Andrianarivo <andrianarivo.david@gmail.com>
2023-08-21 18:22:04 +0300

    implement resource routing

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root 'users#index'
-  get '/users', to: 'users#index'
-  get '/users/:id', to: 'users#show'
+  
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
 
-  get '/users/:id/posts', to: 'posts#index'
-  get '/users/:id/posts/:id', to: 'posts#show'
 end
