19236b73a2a366c770410cf53d062065c5a62912
David Andrianarivo <andrianarivo.david@gmail.com>
2023-08-21 21:14:53 +0300

    implement controllers actions for views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,17 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    page = params[:page] || 1
+    per_page = 10
+
+    @posts = Post.includes(:author)
+                 .where(author: params[:user_id])
+                 .order(created_at: :asc)
+                 .offset((page.to_i - 1) * per_page)
+                 .limit(per_page)
+
+    @total_pages = (Post.count.to_f / per_page).ceil
+    @author = @posts.first.author
+  end
 
   def show; end
 end
