a4e16587ea92db245dee07eec3ea5bd8cbf822c3
David Andrianarivo <andrianarivo.david@gmail.com>
2023-08-21 18:12:01 +0300

    implement explicit users#index

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root 'users#index'
+  get '/users', to: 'users#index'
   get '/users/:id', to: 'users#show'
 
   get '/users/:id/posts', to: 'posts#index'
