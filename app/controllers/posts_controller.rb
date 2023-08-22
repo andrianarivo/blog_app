9ff89ecff6829d882f6b22051845d6040c802fc8
David Andrianarivo <andrianarivo.david@gmail.com>
2023-08-22 10:25:50 +0300

    rubocop

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,11 +4,11 @@ class PostsController < ApplicationController
     per_page = 10
 
     @posts = Post.includes(:author)
-                 .includes(:comments)
-                 .where(author: params[:user_id])
-                 .order(created_at: :asc)
-                 .offset((page.to_i - 1) * per_page)
-                 .limit(per_page)
+      .includes(:comments)
+      .where(author: params[:user_id])
+      .order(created_at: :asc)
+      .offset((page.to_i - 1) * per_page)
+      .limit(per_page)
 
     @total_pages = (Post.count.to_f / per_page).ceil
     @author = @posts.first.author
