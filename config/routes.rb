48af854925515565b4d6e2db4eb84bbd35493ebb
David Andrianarivo <andrianarivo.david@gmail.com>
2023-08-23 17:36:44 +0300

    build post new create form

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   root 'users#index'
 
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :show, :create, :new]
   end
 
 end
