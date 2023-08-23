6e4258ef6cdd4e3d8e98acfdd7bbda1238b366d7
David Andrianarivo <andrianarivo.david@gmail.com>
2023-08-23 17:37:41 +0300

    build comment new create form

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,6 @@ Rails.application.routes.draw do
     resources :posts, only: [:index, :show, :create, :new]
   end
 
+  get 'users/:user_id/posts/:post_id/comments/new', to: 'comments#new'
+  post 'users/:user_id/posts/:post_id/comments', to: 'comments#create', as: 'comments'
 end
