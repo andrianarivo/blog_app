1cd049781b9743259772a44236b847007df33d1b
andrianarivo <andrianarivo.david@gmail.com>
2023-08-29 01:58:13 +0300

    posts#show system tests

--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -76,4 +76,44 @@ RSpec.describe 'Posts', type: :system do
     end
   end
 
+  describe 'posts#show' do
+    it 'I can see the post\'s title.' do
+      visit "/users/#{@tom.id}/posts/#{@first_post.id}"
+      expect(page).to have_content(@first_post.title)
+    end
+
+    it 'I can see who wrote the post.' do
+      visit "/users/#{@tom.id}/posts/#{@first_post.id}"
+      expect(page).to have_content(@tom.name)
+    end
+
+    it 'I can see how many comments it has.' do
+      visit "/users/#{@tom.id}/posts/#{@first_post.id}"
+      expect(page).to have_content("Comments: #{@first_post.comments_counter},")
+    end
+
+    it 'I can see how many likes it has.' do
+      visit "/users/#{@tom.id}/posts/#{@first_post.id}"
+      expect(page).to have_content("Likes: #{@first_post.likes_counter}")
+    end
+
+    it 'I can see the post body.' do
+      visit "/users/#{@tom.id}/posts/#{@first_post.id}"
+      expect(page).to have_content(@first_post.text)
+    end
+
+    it 'I can see the username of each commentor.' do
+      visit "/users/#{@tom.id}/posts/#{@first_post.id}"
+      @first_post.comments.each do |comment|
+        expect(page).to have_content(comment.author.name)
+      end
+    end
+
+    it 'I can see the comment each commentor left.' do
+      visit "/users/#{@tom.id}/posts/#{@first_post.id}"
+      @first_post.comments.each do |comment|
+        expect(page).to have_content(comment.text)
+      end
+    end
+  end
 end
