657b3f0abec8c44adf8d67127f30997514db5706
andrianarivo <andrianarivo.david@gmail.com>
2023-08-28 22:27:19 +0300

    users#index system test

--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Posts", type: :system do
+RSpec.describe 'Posts', type: :system do
   before do
     driven_by(:rack_test)
   end
--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,9 +1,40 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :system do
-  before do
-    driven_by(:rack_test)
+RSpec.describe 'Users', type: :system do
+  before(:all) do
+    @users = User.all
+    if @users.nil? || @users.empty?
+      User.create(name: 'Tom', photo: 'https://placehold.co/200x133', bio: 'Teacher from Mexico.', posts_counter: 0)
+      User.create(name: 'Lilly', photo: 'https://placehold.co/200x133', bio: 'Teacher from Poland.', posts_counter: 0)
+      @users = User.all
+    end
   end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  describe 'users#index' do
+    it 'I can see the username of all other users.' do
+      visit '/users'
+      expect(page).to have_content('Tom')
+      expect(page).to have_content('Lilly')
+    end
+
+    it 'I can see the profile picture for each user.' do
+      visit '/users'
+      @users.each do
+        expect(page).to have_css("img[src='https://placehold.co/200x133']")
+      end
+    end
+
+    it 'I can see the number of posts each user has written.' do
+      visit '/users'
+      @users.each do
+        expect(page).to have_content('Number of posts: 0')
+      end
+    end
+
+    it 'When I click on a user, I am redirected to that user\'s show page.' do
+      visit '/users'
+      click_link 'Tom'
+      expect(page).to have_current_path("/users/#{@users[0].id}")
+    end
+  end
 end
