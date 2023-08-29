36418fe0e2de22e4e2900163d8a619b9d075714e
Amarachi Dimkpa <amaradimkpa1@gmail.com>
2023-08-29 05:14:08 -0400

    Add the show test

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -37,4 +37,34 @@ RSpec.describe 'Users', type: :system do
       expect(page).to have_current_path("/users/#{@users[0].id}")
     end
   end
+
+ describe 'users#show' do
+    it 'I can see the user\s profile picture..' do
+      visit '/users/1'
+        expect(page).to have_css("img[src='https://placehold.co/200x133']")
+    end
+
+    it 'I can see the user/s username.' do
+      visit '/users/1'
+        expect(page).to have_content("Tom")
+      
+    end
+
+    it 'I can see the number of posts the user has written.' do
+      visit '/users/1'
+        expect(page).to have_content('Number of posts: 0')
+    end
+
+    it 'I can see the user\s bio.' do
+      visit '/users/1'
+      
+      expect(page).to have_content("Teacher from Mexico.")
+    end
+    it 'I can see the user\s first 3 posts.' do
+      visit '/users/1'
+      
+      expect(page).to have_content(" ")
+    end
+  end
 end
+
